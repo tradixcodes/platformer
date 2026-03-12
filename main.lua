c8a9b13112c47bc2279476c228c9e34d1d2fb7a4
tradixcodes <tradixiv@gmail.com>
2026-03-12 14:15:41 +0300

    B: Learning colliders

--- main.lua
+++ main.lua
@@ -1,21 +1,37 @@
 function love.load()
 	wf = require("library/windfield/windfield")
-	world = wf.newWorld(0, 800)
+	world = wf.newWorld(0, 800, false)
+	-- sleeping is the gravity not being calculated
 
-	player = world:newRectangleCollider(360, 100, 80, 80)
+	world:addCollisionClass("Platform")
+	world:addCollisionClass("Player"--[[, { ignores = { "Platform" } }]])
+	world:addCollisionClass("Danger")
+
+	player = world:newRectangleCollider(360, 100, 80, 80, { collision_class = "Player" })
+	player:setFixedRotation(true)
 	player.speed = 240
-	platform = world:newRectangleCollider(250, 400, 300, 100)
+
+	platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = "Platform" })
 	platform:setType("static")
+
+	dangerZone = world:newRectangleCollider(0, 550, 800, 50, { collision_class = "Danger" })
+	dangerZone:setType("static")
 end
 
 function love.update(dt)
 	world:update(dt)
 
-	local px, py = player:getPosition()
-	if love.keyboard.isDown("right") then
-		player:setX(px + player.speed * dt)
-	elseif love.keyboard.isDown("left") then
-		player:setX(px - player.speed * dt)
+	if player.body then
+		local px, py = player:getPosition()
+		if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+			player:setX(px + player.speed * dt)
+		elseif love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+			player:setX(px - player.speed * dt)
+		end
+
+		if player:enter("Danger") then
+			player:destroy()
+		end
 	end
 end
 
@@ -24,7 +40,7 @@ function love.draw()
 end
 
 function love.keypressed(key)
-	if key == "up" then
+	if key == "up" or key == "w" then
 		player:applyLinearImpulse(0, -6500)
 	end
 end
